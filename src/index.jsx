471dfb8987629206e2aead4a47be60e89084ea81
Marlon Tancredo <marlon_tancredo@hotmail.com>
2021-10-18 22:12:27 +0100

    apaguei pasta do web reports

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Calculator from "./main/Calculator";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculator from './main/Calculator';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,10 +10,5 @@ ReactDOM.render(
       <Calculator />
     </div>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
