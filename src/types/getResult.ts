a4102081bd58826739c44d3e242241f2067b01ba
Marlon Tancredo <marlon_tancredo@hotmail.com>
2021-10-18 23:07:46 +0100

    modifiquei types

--- src/types/getResult.ts
+++ src/types/getResult.ts
@@ -2,13 +2,13 @@ const getResult = (values: number[], op: string | null) => {
   const result = {
     res: 0,
   };
-  if (op === "+") {
+  if (op === '+') {
     result.res = values[0] + values[1];
-  } else if (op === "-") {
+  } else if (op === '-') {
     result.res = values[0] - values[1];
-  } else if (op === "*") {
+  } else if (op === '*') {
     result.res = values[0] * values[1];
-  } else if (op === "/") {
+  } else if (op === '/') {
     result.res = values[0] / values[1];
   }
   return result.res;
